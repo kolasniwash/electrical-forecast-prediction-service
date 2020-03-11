b55e843618c94e7603654522b76dd576522afc79
Nicholas <nicholas@nicholasjhana.com>
2020-03-11 19:38:11 +0100

    bug fixes

--- cloud_functions/gen_persistance_forecasts.py
+++ cloud_functions/gen_persistance_forecasts.py
@@ -17,6 +17,7 @@ Types of persistance forecasts available:
 def gen_persistance_forecasts(request):
     from datetime import datetime, timedelta
     import pandas as pd
+    import numpy as np
     from google.cloud import storage
     
     FOLDER_DOWN = 'raw-days'
@@ -54,10 +55,10 @@ def gen_persistance_forecasts(request):
         bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(f'{folder_name}/{file_name}')
         data_json = blob.download_as_string()
- 
+        
         return pd.read_json(data_json, typ='series', orient='records', keep_default_dates=False)
     
-    def upload_data_to_gcs(client, data, date, bucket_name, folder_name, file_name):
+    def upload_data_to_gcs(client, data, bucket_name, folder_name, file_name):
         
         bucket = client.get_bucket(bucket_name)
         blob = bucket.blob(f'{folder_name}/{file_name}')
@@ -71,65 +72,61 @@ def gen_persistance_forecasts(request):
         return data
 
 
-    def persistance(series):
-        date = (datetime.today()+timedelta(-1)).strftime('%Y%m%d')
+    def persistance(series, date):
         return series[date]
 
-
-    def persistance_day_ma(series, num_days):
+    def persistance_day_ma(series, num_days, date):
+    
         window=24*num_days
         rolling_mean = series.rolling(window=window, min_periods=24, closed='right').mean()
     
-        return rolling_mean[-24:]
+        return rolling_mean[date]
 
     def persistance_MA_hourly(series, days):
         
-        df = pd.DataFrame(series.values.reshape((days,24)))
-        mean = df.mean()
-        mean.index = series.index[-24:]
+        df = series.groupby(series.index.hour).mean()
     
-        return mean
+        return df
 
     def calc_persistance_forecasts(data):
-        p1 = persistance(data)
-        p2 = persistance_day_ma(data, 3)
-        p3 = persistance_MA_hourly(data, 3)
-
-        persist_forecast = pd.concat([p1, p2, p3], axis=1)
-        persist_forecast.columns = ['naive', 'MA3-day', 'MA30day-hbh']
-
-        date = datetime.today().strftime('%Y%m%d')
-        persist_forecast.index = pd.DatetimeIndex(pd.date_range(start=f'{date}T0000', end=f'{date}T2300', freq='H'))
+        date = (datetime.today()+timedelta(-1)).strftime('%Y%m%d')
         
-        return persist_forecast
+        p1 = persistance(data, date)
+        p2 = persistance_day_ma(data, 3, date)
+        p3 = persistance_MA_hourly(data, 3)
+        
+        data = np.vstack([p1.values, p2.values, p3.values]).T
+        persist_df = pd.DataFrame(data, columns=['naive', 'MA3-day', 'MA30day-hbh'])
+
+        today = datetime.today().strftime('%Y%m%d')
+        persist_df.index = pd.DatetimeIndex(pd.date_range(start=f'{today}T0000', end=f'{today}T2300', freq='H'))
+        
+        return persist_df
 
     params = request.get_json()
     
     if 'gen_persist' in params and params['gen_persist']:
-        #storage_client = storage.Client()
+        storage_client = storage.Client()
 
-        storage_client = service_account.Credentials.from_service_account_file(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
-
+        # creds = service_account.Credentials.from_service_account_file(os.environ['GOOGLE_APPLICATION_CREDENTIALS'])
+        # storage_client = storage.Client(credentials=creds, project=project_id)
         #download the data to make the persistance forecast
         time_pairs = get_time_dates(3)
-
+        print(time_pairs)
         data_list = list()
 
         for time_pair in time_pairs:
 
             file_name = f'es-energy-demand-{time_pair[0]}-{time_pair[1]}'
             data = get_gcs_data(storage_client, BUCKET, FOLDER_DOWN, file_name)
-
             data_list.append(data)
-
-
+            
         data = reset_data_index(data_list)
 
         #calcuate the persistance forecasts
-        persistance_forecasts = calc_persistance_forecats(data)
+        persistance_forecasts = calc_persistance_forecasts(data)
 
-        return persistance_forecasts
         #upload to gcs
-        #persistance_file_name = gcs_save_name(datetime.today())
+        persistance_file_name = gcs_save_name(datetime.today().strftime('%Y%m%d'))
         #create persistance - simple persistance today's values >> tomorrow forecast
-        #upload_data_to_gcs(storage_client, data, date_to_forecast, BUCKET, FOLDER_UP, file_to_forecast)
+        upload_data_to_gcs(storage_client, persistance_forecasts, BUCKET, FOLDER_UP, persistance_file_name)
